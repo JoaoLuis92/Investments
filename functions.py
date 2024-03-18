34f3fe0f110a919b84bfec606cf0a26ed0461ac9
João Luís Rosa <joaoluis92@gmail.com>
2024-03-18 19:03:15 +0000

    Added first financial indicators and analysis of current market conditions

---
+++ functions.py
@@ -0,0 +1,43 @@
+# FINANCIAL INDICATORS
+
+# Simple moving average
+
+def simple_moving_average(df, period):
+
+    df["SMA"+str(period)] = df["Close"].rolling(period).mean()
+
+
+# Exponential moving average
+
+def exponential_moving_average(df, period):
+
+    df["EMA"+str(period)] = df['Close'].ewm(span = period).mean()
+
+# Full Stochastic Oscillator
+
+def full_stochastic(df, fk_period, sk_period, sd_period):
+
+    fast_k_list = []
+
+    for i in range(len(df)):
+        low = df.iloc[i]['Low']
+        high = df.iloc[i]['High']
+
+        if i >= fk_period:
+
+            for n in range(fk_period):
+
+                if df.iloc[i-n]['High'] >= high:
+                    high = df.iloc[i-n]['High']
+                elif df.iloc[i-n]['Low'] < low:
+                    low = df.iloc[i-n]['Low']
+        if high != low:
+            fast_k = 100 * (df.iloc[i]['Close'] - low) / (high - low)
+        else:
+            fast_k = 0
+
+        fast_k_list.append(fast_k)
+
+    df["Fast K"] = fast_k_list
+    df["Slow K"] = df["Fast K"].rolling(sk_period).mean()
+    df["Slow D"] = df["Slow K"].rolling(sd_period).mean()
