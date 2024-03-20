21d9d26fe7e458a85b3fa9c236689e7da05dadbe
João Luís Rosa <joaoluis92@gmail.com>
2024-03-20 10:01:49 +0000

    Added first set of candlestick patterns

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 # I need to separate this between functions to compute the indicators and functions to check the signals.
 # for example, check trend signal, check oscillator signal, etc, which make use of the financial indicators!
 
+import numpy as np
 
 # Simple moving average
 
@@ -44,3 +45,273 @@ def full_stochastic(df, fk_period, sk_period, sd_period):
     df["Fast K"] = fast_k_list
     df["Slow K"] = df["Fast K"].rolling(sk_period).mean()
     df["Slow D"] = df["Slow K"].rolling(sd_period).mean()
+
+# CANDLESTICK PATTERNS
+
+def body_and_range(df):
+
+    df["Body"] = np.absolute(df["Open"] - df["Close"])
+    df["Range"] = df["High"] - df["Low"]
+
+def pattern_confirmation_bullish(df):
+
+    confirmation_day = df.iloc[len(df)-1]
+    last_pattern_day = df.iloc[len(df)-2]
+
+    confirmation_1 = confirmation_day.Low > last_pattern_day.Low
+    confirmation_2 = confirmation_day.Close > last_pattern_day.High
+    confirmation_3 = confirmation_day.Close > confirmation_day.Open
+
+    return confirmation_1 and confirmation_2 and confirmation_3
+
+def pattern_confirmation_bearish(df):
+
+    confirmation_day = df.iloc[len(df)-1]
+    last_pattern_day = df.iloc[len(df)-2]
+
+    confirmation_1 = confirmation_day.High < last_pattern_day.High
+    confirmation_2 = confirmation_day.Close < last_pattern_day.Low
+    confirmation_3 = confirmation_day.Close < confirmation_day.Open
+
+    return confirmation_1 and confirmation_2 and confirmation_3
+
+
+def pattern_bullish_pinbar(df, confirmation=True):
+
+    df_work = df.tail(2)
+    body_and_range(df_work)
+
+    if confirmation:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-2]
+        confirmation_day = df_work.iloc[len(df_work)-1]
+
+    else:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-1]
+
+    requirement_1 = pattern_day_1.Body < pattern_day_1.Range / 3
+    requirement_2 = min(pattern_day_1.Open, pattern_day_1.Close) > pattern_day_1.Low + (2/3) * pattern_day_1.Range
+
+    if confirmation:
+
+        requirement_3 = pattern_confirmation_bullish(df_work)
+        return requirement_1 and requirement_2 and requirement_3
+
+    else:
+
+        return requirement_1 and requirement_2
+
+def pattern_bearish_pinbar(df, confirmation=True):
+
+    df_work = df.tail(2)
+    body_and_range(df_work)
+
+    if confirmation:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-2]
+        confirmation_day = df_work.iloc[len(df_work)-1]
+
+    else:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-1]
+
+    requirement_1 = pattern_day_1.Body < pattern_day_1.Range / 3
+    requirement_2 = max(pattern_day_1.Open, pattern_day_1.Close) < pattern_day_1.High - (2/3) * pattern_day_1.Range
+
+    if confirmation:
+
+        requirement_3 = pattern_confirmation_bearish(df_work)
+        return requirement_1 and requirement_2 and requirement_3
+
+    else:
+
+        return requirement_1 and requirement_2
+
+def pattern_white_soldier(df, confirmation=True):
+
+    df_work = df.tail(3)
+
+    if confirmation:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-3]
+        pattern_day_2 = df_work.iloc[len(df_work)-2]
+        confirmation_day = df_work.iloc[len(df_work)-1]
+
+    else:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-2]
+        pattern_day_2 = df_work.iloc[len(df_work)-1]
+
+    requirement_1 = pattern_day_2.Low > pattern_day_1.Low
+    requirement_2 = pattern_day_2.Open > pattern_day_1.Close
+    requirement_3 = pattern_day_2.Close > pattern_day_1.High
+    requirement_4 = pattern_day_1.Close < pattern_day_1.Open
+    requirement_5 = pattern_day_2.Close > pattern_day_2.Open
+
+    if confirmation:
+
+        requirement_6 = pattern_confirmation_bullish(df_work)
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5 and requirement_6
+
+    else:
+
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5
+
+def pattern_black_crow(df, confirmation=True):
+
+    df_work = df.tail(3)
+
+    if confirmation:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-3]
+        pattern_day_2 = df_work.iloc[len(df_work)-2]
+        confirmation_day = df_work.iloc[len(df_work)-1]
+
+    else:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-2]
+        pattern_day_2 = df_work.iloc[len(df_work)-1]
+
+    requirement_1 = pattern_day_2.High < pattern_day_1.High
+    requirement_2 = pattern_day_2.Open < pattern_day_1.Close
+    requirement_3 = pattern_day_2.Close < pattern_day_1.Low
+    requirement_4 = pattern_day_1.Close > pattern_day_1.Open
+    requirement_5 = pattern_day_2.Close < pattern_day_2.Open
+
+    if confirmation:
+
+        requirement_6 = pattern_confirmation_bearish(df_work)
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5 and requirement_6
+
+    else:
+
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5
+
+def pattern_morning_star(df, confirmation=True):
+
+    df_work = df.tail(4)
+    body_and_range(df_work)
+
+    if confirmation:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-4]
+        pattern_day_2 = df_work.iloc[len(df_work)-3]
+        pattern_day_3 = df_work.iloc[len(df_work)-2]
+        confirmation_day = df_work.iloc[len(df_work)-1]
+
+    else:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-3]
+        pattern_day_2 = df_work.iloc[len(df_work)-2]
+        pattern_day_3 = df_work.iloc[len(df_work)-1]
+
+    requirement_1 = pattern_day_2.Body < 0.5 * pattern_day_1.Body
+    requirement_2 = pattern_day_2.Body < 0.5 * pattern_day_3.Body
+    requirement_3 = max(pattern_day_2.Open,pattern_day_2.Close) < pattern_day_1.Close
+    requirement_4 = max(pattern_day_2.Open,pattern_day_2.Close) < pattern_day_3.Open
+    requirement_5 = pattern_day_1.Close < pattern_day_1.Open
+    requirement_6 = pattern_day_3.Close > pattern_day_3.Open
+    requirement_7 = pattern_day_3.Close > pattern_day_1.Close + 0.5 * pattern_day_1.Body
+
+    if confirmation:
+
+        requirement_8 = pattern_confirmation_bullish(df_work)
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5 and requirement_6 and requirement_7 and requirement_8
+
+    else:
+
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5 and requirement_6 and requirement_7
+
+def pattern_evening_star(df, confirmation=True):
+
+    df_work = df.tail(4)
+    body_and_range(df_work)
+
+    if confirmation:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-4]
+        pattern_day_2 = df_work.iloc[len(df_work)-3]
+        pattern_day_3 = df_work.iloc[len(df_work)-2]
+        confirmation_day = df_work.iloc[len(df_work)-1]
+
+    else:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-3]
+        pattern_day_2 = df_work.iloc[len(df_work)-2]
+        pattern_day_3 = df_work.iloc[len(df_work)-1]
+
+    requirement_1 = pattern_day_2.Body < 0.5 * pattern_day_1.Body
+    requirement_2 = pattern_day_2.Body < 0.5 * pattern_day_3.Body
+    requirement_3 = min(pattern_day_2.Open,pattern_day_2.Close) > pattern_day_1.Close
+    requirement_4 = min(pattern_day_2.Open,pattern_day_2.Close) > pattern_day_3.Open
+    requirement_5 = pattern_day_1.Close > pattern_day_1.Open
+    requirement_6 = pattern_day_3.Close < pattern_day_3.Open
+    requirement_7 = pattern_day_3.Close < pattern_day_1.Close - 0.5 * pattern_day_1.Body
+
+    if confirmation:
+
+        requirement_8 = pattern_confirmation_bearish(df_work)
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5 and requirement_6 and requirement_7 and requirement_8
+
+    else:
+
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5 and requirement_6 and requirement_7
+
+def pattern_bullish_engulfing(df, confirmation=True):
+
+    df_work = df.tail(3)
+
+    if confirmation:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-3]
+        pattern_day_2 = df_work.iloc[len(df_work)-2]
+        confirmation_day = df_work.iloc[len(df_work)-1]
+
+    else:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-2]
+        pattern_day_2 = df_work.iloc[len(df_work)-1]
+
+    requirement_1 = pattern_day_1.Close < pattern_day_1.Open
+    requirement_2 = pattern_day_2.Close > pattern_day_2.Open
+    requirement_3 = pattern_day_2.Open < pattern_day_1.Close
+    requirement_4 = pattern_day_2.Close > pattern_day_1.High
+
+    if confirmation:
+
+        requirement_5 = pattern_confirmation_bullish(df_work)
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5
+
+    else:
+
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4
+
+def pattern_bearish_engulfing(df, confirmation=True):
+
+    df_work = df.tail(3)
+
+    if confirmation:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-3]
+        pattern_day_2 = df_work.iloc[len(df_work)-2]
+        confirmation_day = df_work.iloc[len(df_work)-1]
+
+    else:
+
+        pattern_day_1 = df_work.iloc[len(df_work)-2]
+        pattern_day_2 = df_work.iloc[len(df_work)-1]
+
+    requirement_1 = pattern_day_1.Close > pattern_day_1.Open
+    requirement_2 = pattern_day_2.Close < pattern_day_2.Open
+    requirement_3 = pattern_day_2.Open > pattern_day_1.Close
+    requirement_4 = pattern_day_2.Close < pattern_day_1.Low
+
+    if confirmation:
+
+        requirement_5 = pattern_confirmation_bearish(df_work)
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4 and requirement_5
+
+    else:
+
+        return requirement_1 and requirement_2 and requirement_3 and requirement_4
